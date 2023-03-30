704d84bbf6d3d6b0c636020b5214f9d57aeb8252
ashik190141 <ashanmahmudashik@gmail.com>
2023-03-30 13:09:37 +0600

    cardset

---
+++ src/component/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (proms) => {
+    const { img, name, price, seller, ratings } = proms.product;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='info'>
+                <p className='product-name'>{name}</p>
+                <p className='price'>Price: ${price}</p>
+                <p className='fontsize'>Manufacture: {seller}</p>
+                <p className='fontsize'>Rating: {ratings} stars</p>
+            </div>
+            <button className='btn'>Add To Cart
+            <FontAwesomeIcon icon={faShoppingCart} className="cartIcon" />
+            </button>
+        </div>
+    );
+};
+
+export default Product;
